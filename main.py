07e2be6340ceafd0a615f10f78d1619f0c9537f2
david <davidlima@ufba.br>
2025-12-12 16:17:02 -0300

    feat: Accept newton's method

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from benchmark import InterpBenchmark
 from lagrange import lagrange, lagrange_parallel, lagrange_parallel_batch
+from newton import ReconstrutorNewton
 
 def main():
     ks = [5, 10, 20, 50, 100]
-    interp_fns = [lagrange, lagrange_parallel]
+    interp_fns = [lagrange, lagrange_parallel, ReconstrutorNewton.run_newton]
 
     bench = InterpBenchmark()
     bench.run(
